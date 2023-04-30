6d5baa9292ef7ca7532a2c77b86ac3b8fbb763b6
Njinu <njinukimani@gmail.com>
2023-04-30 20:06:39 +0200

    feat

--- js/main.js
+++ js/main.js
@@ -9,23 +9,56 @@ $(function() {
             margin: 20,
             smartSpeed: 1000,
             autoplay: true,
-            nav: true,
-            dots: true,
+            nav: false,
+            dots: false,
             pauseOnHover: false,
             responsive:{
                 600:{
                     margin: 20,
-                    nav: true,
+                    nav: false,
                   items: 2
                 },
                 1000:{
                     margin: 20,
                     stagePadding: 0,
-                    nav: true,
+                    nav: false,
                   items: 3
                 }
             }
-        });            
+        });        
+            
     }
+
+    if ( $('.owl-1').length > 0 ) {
+      $('.owl-1').owlCarousel({
+          center: false,
+          items: 1.5,
+          loop: false,
+          stagePadding: 0,
+          margin: 20,
+          smartSpeed: 1000,
+          autoplay: false,
+          nav: true,
+          dots: true,
+          navContainer: '#owl-nav',
+          pauseOnHover: false,
+          responsive:{
+              600:{
+                  margin: 20,
+                  nav: true,
+                items: 2
+              },
+              1000:{
+                  margin: 20,
+                  stagePadding: 0,
+                  nav: true,
+                items: 3
+              }
+          }
+      });            
+  }
+
+
+    
 
 })
