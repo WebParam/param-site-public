278e549b67236a3a8f21d2d40ec6f5c6900579ff
Njinu <njinukimani@gmail.com>
2023-05-01 10:15:22 +0200

    feat(up)

--- js/main.js
+++ js/main.js
@@ -58,6 +58,64 @@ $(function() {
       });            
   }
 
+  if ( $('.owl-3').length > 0 ) {
+    $('.owl-3').owlCarousel({
+        center: false,
+        items: 1,
+        loop: true,
+        stagePadding: 0,
+        margin: 20,
+        smartSpeed: 1000,
+        autoplay: false,
+        nav: true,
+        dots: true,
+        navContainer: '#owl-nav3',
+        pauseOnHover: false,
+        responsive:{
+            600:{
+                margin: 20,
+                nav: true,
+              items: 2
+            },
+            1000:{
+                margin: 20,
+                stagePadding: 0,
+                nav: true,
+              items: 2.5
+            }
+        }
+    });            
+}
+
+if ( $('.owl-4').length > 0 ) {
+  $('.owl-4').owlCarousel({
+      center: false,
+      items: 1,
+      loop: true,
+      stagePadding: 0,
+      margin: 20,
+      smartSpeed: 1000,
+      autoplay: false,
+      nav: true,
+      dots: true,
+      navContainer: '#owl-nav4',
+      pauseOnHover: false,
+      responsive:{
+          600:{
+              margin: 20,
+              nav: true,
+            items: 2
+          },
+          1000:{
+              margin: 20,
+              stagePadding: 0,
+              nav: true,
+            items: 3
+          }
+      }
+  });            
+}
+
 
     
 
