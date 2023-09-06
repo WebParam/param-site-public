e3a52f56e5c2c565e7309cef2d9704b756e1c0b6
atlegangkola <141028211+atlegangkola@users.noreply.github.com>
2023-09-06 11:59:01 +0200

    updated forms.html

--- form.js
+++ form.js
@@ -10,22 +10,16 @@ myForm.addEventListener('submit', function (e) {
         method: 'POST',
         body: formData,
     })
-    .then(function(response) {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json(); 
-    })
     .then(function(data) {
-        console.log('Response from the API:', data);
-        
-       
-        if (data.success) {
-            window.location.href = '/success.html'; 
-        } else {
-            
-            console.error('API response indicates failure');
+        debugger;
+        if (!data.ok) {
+            throw new Error('Network response was not ok'); 
+            // return response.json(); 
         }
+       
+        console.log('Response from the API:', data);
+        window.location.href = 'success.html'; 
+      
     })
     .catch(function(error) {
         console.error('Error:', error);
