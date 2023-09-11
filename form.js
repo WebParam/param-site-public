202f4019a03117872367509fcb8bb9d1fe37fff2
atlegangkola <141028211+atlegangkola@users.noreply.github.com>
2023-09-11 10:10:12 +0200

    Integrated with monday.com sucessfully

--- form.js
+++ form.js
@@ -1,28 +1,46 @@
 const myForm1 = document.getElementById('myForm1');
 
-
 myForm1.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const formData = new FormData(this);
+    // Create an object and populate it with form input values
+    const formDataObject = {};
+    const formElements = this.elements;
+
+    for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+        if (element.name) {
+            formDataObject[element.name] = element.value;
+        }
+    }
+
+    formDataObject["date"] = new Date().toISOString().slice(0, 10);
+
+    
+
+
+    // Convert the object to JSON
+    const jsonData = JSON.stringify(formDataObject);
 
     fetch('https://eo9jatrnyl5bx9g.m.pipedream.net', {
         method: 'POST',
-        body: formData,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: jsonData,
     })
     .then(function(data) {
-        debugger;
         if (!data.ok) {
             throw new Error('Network response was not ok'); 
-            // return response.json(); 
         }
        
         console.log('Response from the API:', data);
-        window.location.href = 'success.html'; 
+        console.log(formDataObject.email);
+
+         window.location.href = 'success.html'; 
       
     })
     .catch(function(error) {
         console.error('Error:', error);
     });
 });
-
